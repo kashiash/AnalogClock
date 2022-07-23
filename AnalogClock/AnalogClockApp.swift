f52418ebd19c064d2b30f598a46edb5b0ce7cc59
kashiash <kashiash@gmail.com>
2022-07-23 12:10:16 +0200

    Initial Commit

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by Jacek Placek on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
